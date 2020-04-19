8e177b4e9f5b4baa1b516ad93cd6b9590a859aed
Lucas deGomez <lucasdegomez@gmail.com>
2020-04-19 20:58:52 +0200

    Webpack sourcemaps for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: {
     bundle: './src/index.js',
   },
