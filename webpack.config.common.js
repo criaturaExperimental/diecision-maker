502a5c8cc48c761456960eb7a33dc26d283db164
Lucas deGomez <lucasdegomez@gmail.com>
2020-05-09 17:12:27 +0200

    remove cleaning dist in local

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -27,7 +26,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
