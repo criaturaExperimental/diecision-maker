27e76ea3eeba4d4123b1c5d3fcb4b547e0f801bb
Lucas deGomez <lucasdegomez@gmail.com>
2020-05-09 15:00:26 +0200

    Manifest support

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -13,6 +14,7 @@ module.exports = merge(common, {
       inject: true,
       template: './src/index.html',
       filename: './index.html',
+      favicon: './src/assets/favicon.ico',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -30,6 +32,27 @@ module.exports = merge(common, {
       swDest: path.resolve('dist', 'sw.js'),
       clientsClaim: true,
       skipWaiting: true,
+    }),
+    new WebpackPwaManifest({
+      name: 'Diecision Maker',
+      short_name: 'DiecisionMaker',
+      description: 'Let the die decide',
+      background_color: '#000',
+      theme_color: '#000',
+      'theme-color': '#000',
+      crossorigin: 'use-credentials',
+      fingerprints: false,
+      inject: true,
+      icons: [
+        {
+          src: path.resolve('src/assets/logo192.png'),
+          sizes: '192x192',
+        },
+        {
+          src: path.resolve('src/assets/logo512.png'),
+          sizes: '512x512',
+        },
+      ],
     }),
   ],
 });
