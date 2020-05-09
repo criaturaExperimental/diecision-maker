8de14ab9e593f367604efae2c9c1a50045e50f40
Lucas deGomez <lucasdegomez@gmail.com>
2020-05-09 14:26:49 +0200

    Provide service worker with workbox

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import ReactDOM from 'react-dom';
 import { App } from 'components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('App'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,35 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: false,
+      inject: true,
+      template: './src/index.html',
+      filename: './index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new WorkboxPlugin.GenerateSW({
+      swDest: path.resolve('dist', 'sw.js'),
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
 });
